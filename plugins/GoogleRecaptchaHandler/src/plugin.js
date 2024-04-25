f4de7a37ed504561c31a834080f58e65a00118aa
Marc S. Brooks <marc@mbrooks.info>
2024-04-25 13:43:17 -0700

    Refactor, added Error exception assert
    Installed chai-as-promised (again)

--- plugins/GoogleRecaptchaHandler/src/plugin.js
+++ plugins/GoogleRecaptchaHandler/src/plugin.js
@@ -2,60 +2,56 @@
 
 const https = require('https');
 
+const {RouterError} = require('@lambda-lambda-lambda/router/src/router/Error');
+
 /**
  * Middleware to validate Google ReCAPTCHA invisible responses.
  *
  * @requires AppConfigPlugin
  */
 module.exports = async (req, res, next) => {
-  return new Promise(function(resolve) {
-    const {google} = req.plugin('config');
+  const {google} = req.plugin('config');
 
-    const secretKey = google?.reCaptcha?.secretKey;
+  const secretKey = google?.reCaptcha?.secretKey;
 
-    if (!secretKey) {
-      /* istanbul ignore next */
-      throw new Error('Missing Google API secret key');
-    }
+  if (!secretKey) {
+    throw new RouterError('Missing Google API secret key');
+  }
 
-    if (req.method() === 'POST') {
-      const captchaResponse = req.param('g-recaptcha-response');
+  /* istanbul ignore next */
+  if (req.method() === 'POST') {
+    const captchaResponse = req.param('g-recaptcha-response');
 
-      const options = {
-        hostname: 'www.google.com',
-        port: 443,
-        path: '/recaptcha/api/siteverify',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      };
+    const options = {
+      hostname: 'www.google.com',
+      port: 443,
+      path: '/recaptcha/api/siteverify',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
 
-      const httpRequest = https.request(options, hr => {
+    const httpRequest = https.request(options, hr => {
+      if (hr.statusCode === 200) {
+        hr.on('data', buffer => {
+          const {success} = JSON.parse(buffer.toString());
 
-        /* istanbul ignore next */
-        if (hr.statusCode === 200) {
-          hr.on('data', buffer => {
-            const {success} = JSON.parse(buffer.toString());
+          if (success !== true) {
 
-            if (success !== true) {
+            // Return error response.
+            res.status(400).send('Bad Request');
+          }
+        });
+      } else {
+        console.warn(`Google API error: ${hr.statusMessage}`);
 
-              // Return error response.
-              res.status(400).send('Bad Request');
-            }
+        // Return error response.
+        res.status(500).send('Internal Server Error');
+      }
+    });
 
-            resolve();
-          });
-        } else {
-          console.warn(`Google API error: ${hr.statusMessage}`);
-
-          // Return error response.
-          res.status(500).send('Internal Server Error');
-        }
-      });
-
-      httpRequest.write(`secret=${secretKey}&response=${captchaResponse}`);
-      httpRequest.end();
-    }
-  });
+    httpRequest.write(`secret=${secretKey}&response=${captchaResponse}`);
+    httpRequest.end();
+  }
 };
--- plugins/GoogleRecaptchaHandler/test/unit.js
+++ plugins/GoogleRecaptchaHandler/test/unit.js
@@ -1,16 +1,26 @@
 'use strict';
 
-const event = require(`${PACKAGE_ROOT}/event.json`);
-const chai  = require('chai');
+const event          = require(`${PACKAGE_ROOT}/event.json`);
+const chai           = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon          = require('sinon');
+const https          = require('https');
+
+chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
 // Load modules.
-const Request    = require('@lambda-lambda-lambda/router/src/router/Request.js');
-const Response   = require('@lambda-lambda-lambda/router/src/router/Response.js');
-const Stack      = require('@lambda-lambda-lambda/router/src/router/Stack.js');
-const Utils      = require('@lambda-lambda-lambda/router/src/router/Utils.js');
-const middleware = require(PLUGIN_ROOT);
+const {RouterError} = require('@lambda-lambda-lambda/router/src/router/Error');
+const Request       = require('@lambda-lambda-lambda/router/src/router/Request.js');
+const Response      = require('@lambda-lambda-lambda/router/src/router/Response.js');
+const Stack         = require('@lambda-lambda-lambda/router/src/router/Stack.js');
+const Utils         = require('@lambda-lambda-lambda/router/src/router/Utils.js');
+const middleware    = require(PLUGIN_ROOT);
+
+afterEach(() => {
+  sinon.restore();
+});
 
 describe('GoogleReCaptchaHandler', function() {
   describe('success', function() {
@@ -33,7 +43,7 @@ describe('GoogleReCaptchaHandler', function() {
     Utils.setFuncName(dependency, 'middleware');
     Utils.setFuncName(middleware, 'middleware');
 
-    const route = function(req, res) {
+    const route = async function(req, res, next) {
       res.status(200).send();
     };
 
@@ -67,6 +77,52 @@ describe('GoogleReCaptchaHandler', function() {
 
     it('should not return body', function() {
       expect(result.body).to.be.undefined;
+    });
+  });
+
+  describe('error', function() {
+    const stack = new Stack();
+
+    const dependency = function(req, res, next) {
+      const config = {
+        google: {
+          reCaptcha: {
+            secretKey: ''
+          }
+        }
+      };
+
+      req.plugin('config', config);
+      next();
+    };
+
+    Utils.setFuncName(dependency, 'middleware');
+    Utils.setFuncName(middleware, 'middleware');
+
+    const route = async function(req, res, next) {
+      res.status(200).send();
+    };
+
+    Utils.setFuncName(route, 'route:index');
+
+    stack.middleware = [dependency, middleware];
+    stack.routes     = route;
+
+    // Define form POST parameters.
+    event.Records[0].cf.request.method = 'POST';
+    event.Records[0].cf.request.uri    = '/path/to/resource';
+    event.Records[0].cf.request.body   = {
+      data: Buffer.from('g-recaptcha-response=GOOGLE_RESPONSE')
+        .toString('base64')
+    };
+
+    const req = new Request(event.Records[0].cf.request, {});
+    const res = new Response({});
+
+    it('should throw RouterError', function() {
+      const result = stack.exec(req, res);
+
+      return expect(result).to.be.rejectedWith(RouterError, /Missing Google API secret key/);
     });
   });
 });
